792c956559d2c2ff07c7bcb5ce153be1f3f33a28
LuzCar <lcarrillofraire@gmail.com>
2020-10-17 12:59:05 -0500

    Primer ejercicio de automatizacion

---
+++ main.py
@@ -0,0 +1,13 @@
+from common.webdriver_factory import create_driver_instance
+from page_objects.HomePage import HomePage
+
+driver = create_driver_instance('chrome')
+home = HomePage(driver)
+home.open()
+home.wait_until_loaded()
+home.click_logo()
+home.wait_until_loaded()
+job_page = home.search_for_a_job()
+job_page.wait_until_loaded()
+job_page.search('Software Engineer', 'Zapopan, Jalisco')
+driver.quit()
